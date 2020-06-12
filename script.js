3e13f172827bd0c4c44e688ef31de426d6bc5722
koichi-inoue <inoue.ko@gmail.com>
2020-06-12 19:30:37 +0900

    update files

--- script.js
+++ script.js
@@ -1,37 +1,67 @@
-function MyFunction(){
-  var obj = document.body;
-  obj.style.backgroundColor = 'red';
+//外部変数の宣言・定義（以下２つの関数で共通して利用）
+var scale = 100;
+
+// 文字を拡大する関数
+function enlarge(){
+   scale *= 1.25;  // scale = scale * 1.25 と同義（現在のscaleを1.25倍）
+   var obj = document.getElementById('ID07');
+   obj.style.fontSize = scale + '%';
 }
 
-function MyFunction2(){
-  var obj = document.getElementById('ABC');
-  obj.style.backgroundColor = 'yellow';
+//文字を縮小する関数
+function reduce(){
+   scale /= 1.25;  // 現在のscaleを1.25で割る
+   var obj = document.getElementById('ID07');
+   obj.style.fontSize = scale + '%';
 }
 
-function MyFunction3(){
-  // lineクラスの把握（情報が line[0], line[1] ,line[2]・・に一括で入る）
-  var lines = document.getElementsByClassName('line');
-  //  i 番目の pタグ部分の背景を赤に
-  for( var i=0; i<lines.length; i++ ){
-    lines[i].style.backgroundColor = 'red';
+// ユーザー入力でターゲット要素の背景色を変更
+function ChangeBGColor() {
+  var obj = document.getElementById('ID08'); // ターゲット要素
+  var col = document.getElementById('COL'); // 入力
+  obj.style.backgroundColor = col.value;
+}
+
+
+// ユーザー入力でターゲット要素の文字列を変更
+function ChangeContent(){
+    var m = document.getElementById('ID09'); // ターゲット要素
+    var n = document.getElementById('NAME'); // 入力
+    var res = 'こんにちは <b>' + n.value + '</b> さん';
+    m.innerHTML = res;
+}
+
+// ２つの値を掛け算するプログラム
+function Calc01() {
+    var ans = document.getElementById('ANS'); // ターゲット要素
+    var x = document.getElementById('X'); // 入力１
+    var y = document.getElementById('Y'); // 入力２
+    ans.value =Number( x.value ) * Number( y.value );
+}
+
+// 総和を求めるプログラム
+function Calc02() {
+  var s = 0;
+  var xs = Number(XS.value);
+  var xe = Number(XE.value);
+
+  // 変数 s に順次値を足し込む
+  for(var i=xs; i<=xe; i++){
+    s = s + i;
   }
-}
-
-function ChangeBGColor( col ){
-  var obj = document.body;
-  obj.style.backgroundColor = col;
+  ANS2.value = s;
 }
 
 
-// 背景色の変更（汎用性が低い）
-function ChangeBGRandomColor( ){
-  var obj = document.body;
-  obj.style.backgroundColor =RandomColor();
+// クリックイベントに反応するイベントリスナーを定義
+window.onload = function() {
+    var btn = document.getElementById('CALC03');
+    btn.addEventListener('click', Calc03, false);
 }
-
-//ランダムな16進6桁のカラーコードを生成（汎用性が高い）
-function RandomColor(){
-    var col = Math.floor( Math.random() * 0xFFFFFF ).toString(16);
-    for( var i = col.length; i<6; i++ ) { col = '0' + col; }
-    return '#' + col;
+// イベントハンドラー
+function Calc03() {
+    var ans = document.getElementById('ANS3');
+    var x = document.getElementById('X3');
+    var y = document.getElementById('Y3');
+    ans.value =Number( x.value ) * Number( y.value );
 }
